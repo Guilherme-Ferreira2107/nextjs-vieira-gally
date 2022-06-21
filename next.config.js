31c44812b71dc17d72adadf3220e3a15f0f2b6a2
Guilherme Ferreira dos Santos <guiferrera@brq.com>
2022-06-21 15:22:15 -0300

    Add new config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif)$/i,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            limit: 8192,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
